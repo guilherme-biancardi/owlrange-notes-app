ea04af8e12af07d500746ccdb2d2b93012cedcc4
Jeverson Mozer <jeversonsilveira@outlook.com>
2022-10-08 22:20:00 -0300

    pinia installation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+
+const pinia = createPinia
+const app = createApp(App)
+
+app.use(pinia)
+app
+
 
 createApp(App).use(router).mount('#app')
