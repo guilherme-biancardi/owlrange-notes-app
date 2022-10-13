654ea4cfd3dca1a4ce497b32ee292f5acf82509f
Jeverson Mozer <jeversonsilveira@outlook.com>
2022-10-13 20:41:25 -0300

    :sparkles: feat(student-material): rename the pages and start the materials page

---
+++ src/components/studentMaterial/FormAddMaterial.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Adicionar material de estudo</h2>
+
+
+
+
+
+
+
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const status = reactive({
+
+})
+
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/studentMaterialView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Material de estudo</h1>
+    <p>Possui um novo material para estudar?</p>
+
+    <router-link to="form-add-material">
+      <button>+ ADICIONAR MATERIAL</button>
+    </router-link>
+
+
+  </div>
+</template>
+
+<script setup>
+import FormAddMaterial from "../components/studentMaterial/FormAddMaterial.vue";
+</script>
+
+<style scoped></style>
