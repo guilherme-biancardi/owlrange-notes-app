45afedba7559792253bffa32e470cd5522d667db
biancardi_ <guilhermerbiancardi@gmail.com>
2022-10-20 09:46:09 -0300

    create/cronogram-goals

---
+++ src/components/utilities/ModalComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="modal-backdrop">
+    <div
+      class="modal"
+      :style="`width: ${width || 'max-content'}px; 
+      height: ${height || 'max-content'};
+      background-color: ${background || 'var(--white)'}`"
+    >
+      <button @click="emit('close')">
+        <IconComponent :icon="'mdi-close'" :size="24"></IconComponent>
+      </button>
+      <div>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import IconComponent from "./IconComponent.vue";
+
+const emit = defineEmits(["close"]);
+
+defineProps({
+  width: Number,
+  height: Number,
+  background: String,
+});
+</script>
+
+<style scoped>
+.modal-backdrop {
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.097);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: 0;
+  top: 0;
+}
+
+.modal {
+  position: relative;
+  padding: 28px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 12px;
+  box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.08);
+}
+
+.modal > button {
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  background-color: transparent;
+  cursor: pointer;
+  color: var(--blue);
+}
+
+.modal > div{
+  margin-top: 12px;
+}
+</style>
--- src/views/studentGoalsView.vue
+++ src/views/studentGoalsView.vue
@@ -7,34 +7,63 @@
         <p>adicionar meta</p>
       </button>
     </div>
-    <CalendarComponent></CalendarComponent>
+    <CalendarComponent
+      @setDayEvents="setDayEvents"
+      :events="[
+        { title: 'teste', date: '19/10/2022', color: 'purple' },
+        { title: 'teste', date: '19/10/2022', color: 'orange' },
+        { title: 'teste', date: '21/10/2022', color: 'orange' },
+        { title: 'teste', date: '31/12/2022', color: 'purple' },
+      ]"
+    ></CalendarComponent>
+    <EventsModal
+      :events="state.dayEvents?.events"
+      :day="state.dayEvents?.day"
+      v-if="state.eventsModalVisibility"
+      @close="state.eventsModalVisibility = false"
+    ></EventsModal>
   </div>
 </template>
 
 <script setup>
-import CalendarComponent from "../components/studentGoals/CalendarComponent.vue";
 import IconComponent from "@/components/utilities/IconComponent.vue";
+import CalendarComponent from "@/components/studentGoals/CalendarComponent.vue";
+import EventsModal from "@/components/studentGoals/EventsModal.vue";
+import { reactive } from "vue";
+
+const state = reactive({
+  dayEvents: {
+    events: [],
+    day: "19/10/2022",
+  },
+  eventsModalVisibility: false,
+});
+
+const setDayEvents = (dayEvents) => {
+  state.dayEvents = dayEvents;
+  state.eventsModalVisibility = true;
+};
 </script>
 
 <style scoped>
-.cronogram{
+.cronogram {
   display: flex;
   flex-direction: column;
-  row-gap: 16px;
+  row-gap: 24px;
   width: 100%;
 }
 
-.cronogram-header{
+.cronogram-header {
   display: flex;
   align-items: center;
   justify-content: space-between;
 }
 
-.cronogram-header h1{
+.cronogram-header h1 {
   font-size: 1.5em;
 }
 
-.cronogram-header button{
+.cronogram-header button {
   display: flex;
   align-items: center;
   padding: 6px 12px;
