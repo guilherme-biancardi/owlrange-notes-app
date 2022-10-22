52787cd5ea1b3d5ab74fefbc6232c6491a921e4c
biancardi_ <guilhermerbiancardi@gmail.com>
2022-10-21 23:05:51 -0300

    update

--- src/router/modules/loginRoute.js
+++ src/router/modules/loginRoute.js
@@ -1,9 +1,14 @@
+import { pinia } from '@/store/index.js';
+import { useUserStore } from '@/store/userStore.js';
 import { createRoute } from './createRoute.js';
+
+const userStore = useUserStore(pinia)
 
 export const loginRoute  = createRoute('/', import('../../views/login/LoginView.vue'), 'app', {
     redirect: '/sign-in',
     children: [
         createRoute('sign-in', import('../../views/login/SignInView.vue'), 'login'),
         createRoute('register', import('../../views/login/RegisterView.vue'), 'login')
-    ]
+    ],
+    beforeEnter: (to, from, next) => userStore.getLoggedIn ? next({ path: 'app' }) : next(),
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="not-found">
+    <div>
+      <h1>Parece que essa página não existe</h1>
+      <p>Verifique o endereço e tente novamente</p>
+    </div>
+    <img
+      src="../assets/not-found.svg"
+      alt="pessoa com deficiência visual atravessando uma rua"
+    />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.not-found,
+.not-found > div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 40px;
+}
+
+.not-found {
+  width: 100%;
+  height: 100vh;
+}
+
+.not-found > div {
+  row-gap: 5px;
+}
+
+.not-found img {
+  max-width: 550px;
+  object-fit: cover;
+  object-position: center;
+}
+
+h1{
+    color: var(--blue);
+}
+
+p{
+    font-size: 1.15em;
+}
+</style>
